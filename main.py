ec4ee490577d52374596badd35453cf159859556
Antonis Nikitakis <a.s.nikitakis@gmail.com>
2015-03-17 01:51:27 +0200

    minor commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import ImageProcessing
 import LoadData
 
 
-names, labels = LoadData.ImageDatasetCreation(csv_name='data/trainLabels.csv', number_of_data=[500, 500])
+names, labels = LoadData.ImageDatasetCreation(csv_name='CSV/trainLabels.csv', number_of_data=[500, 500])
 for i in range(labels.shape[0]):
     if labels[i] == 4:
         labels[i] = 1
@@ -15,7 +15,7 @@ for name in names:
     print 'Processing image %d'%counter    
     counter = counter + 1
     
-    img_name = 'data/resized/%s.jpg'%name
+    img_name = '../data/resized/%s.jpg'%name
     img = ImageProcessing.LoadImage(img_name)
 
     r,g,b = ImageProcessing.SplitImage(img, silence=True)
@@ -26,6 +26,6 @@ for name in names:
 
     res = cv2.resize(cropped_image, (250, 250),  interpolation = cv2.INTER_AREA)
 
-    out_name = 'data/input/%s.jpg'%name
+    out_name = '../data/input/%s.jpg'%name
     ret = cv2.imwrite(out_name, res)
 
