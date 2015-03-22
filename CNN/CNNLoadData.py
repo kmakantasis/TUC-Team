b6e6692732e0324abffed4b0733aa4502e90e9c9
kostas <konst.makantasis@gmail.com>
2015-03-22 13:42:42 +0200

    kostas changes

--- CNN/CNNLoadData.py
+++ CNN/CNNLoadData.py
@@ -45,3 +45,38 @@ def LoadData(names, labels, ratio=0.8):
     rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y), (test_set_x, test_set_y)]
     
     return rval
+    
+    
+def FeatureConstructionData(names, labels):
+
+    print '... loading data'
+
+    dataset_x = np.zeros((names.shape[0], 250*250))
+    dataset_y = np.zeros((names.shape[0]))
+
+    for i in range(names.shape[0]):
+        img_name = '../data/input/%s.jpg'%names[i]
+        img = cv2.imread(img_name, 0)
+        ret,thresh = cv2.threshold(img,1,1,cv2.THRESH_BINARY)
+        img_flat = np.reshape(thresh, (1, -1))
+
+        dataset_x[i,:] = img_flat
+        dataset_y[i] = labels[i]
+
+    
+    
+    train_dataset_x = dataset_x
+    train_dataset_y = dataset_y
+    
+
+    def shared_dataset(data_x, data_y, borrow=True):
+        shared_x = theano.shared(np.asarray(data_x, dtype=theano.config.floatX), borrow=borrow)
+        shared_y = theano.shared(np.asarray(data_y, dtype=theano.config.floatX), borrow=borrow)
+        
+        return shared_x, T.cast(shared_y, 'int32')
+
+    train_set_x, train_set_y = shared_dataset(train_dataset_x, train_dataset_y)
+    
+    rval = [(train_set_x, train_set_y)]
+    
+    return rval
--- CNN/ConvPoolLayer.py
+++ CNN/ConvPoolLayer.py
@@ -8,7 +8,7 @@ from theano.tensor.signal import downsample
 class ConvPoolLayer(object):
     """ Convolution and pooling layer of a Convolutional Neural Network."""
     
-    def __init__(self, rng, input, filter_shape, image_shape, pool_size=(2, 2), activation=T.tanh):
+    def __init__(self, rng, input, filter_shape, image_shape, pool_size=(2, 2), W=None, b=None, activation=T.tanh):
 
         # image_shape[1] and filter_shape[1] correspond 
         # to number of feature maps and must be equal        
@@ -20,14 +20,21 @@ class ConvPoolLayer(object):
         fan_in = np.prod(filter_shape[1:])
         fan_out = ( filter_shape[0] * np.prod(filter_shape[2:]) / np.prod(pool_size) )
         W_bound = np.sqrt( 6. / (fan_in + fan_out) )
-        self.W = theano.shared(value=np.asarray(rng.uniform(low=-W_bound, high=W_bound, size=filter_shape),
-                                            dtype=theano.config.floatX),
-                          name='W',
-                          borrow=True)
         
-        self.b = theano.shared(value=np.zeros( (filter_shape[0],), dtype=theano.config.floatX ), 
-                          name='b', 
-                          borrow=True )
+        if W==None:
+            self.W = theano.shared(value=np.asarray(rng.uniform(low=-W_bound, high=W_bound, size=filter_shape),
+                                                    dtype=theano.config.floatX),
+                                   name='W',
+                                   borrow=True)
+        else:
+            self.W = theano.shared(value=W, name='W')
+            
+        if b==None:
+            self.b = theano.shared(value=np.zeros( (filter_shape[0],), dtype=theano.config.floatX ), 
+                                   name='b', 
+                                   borrow=True )
+        else:
+            self.b= theano.shared(value=b, name='b')
                           
         # convolution of input feature maps with filters
         conv_out = T.nnet.conv2d(input=input, filters=self.W, image_shape=image_shape, filter_shape=filter_shape)
@@ -39,3 +46,7 @@ class ConvPoolLayer(object):
         self.output = activation(pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
         
         self.params = [self.W, self.b]
+        
+        
+#        def cnn_out(self, input):
+#            return self.output
